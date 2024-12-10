d89f987ae568c8530bced14486c0bc436fd79780
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-12-10 09:46:50 -0500

    Extend IdentityUser with custom BlogUser Model properties

---
+++ Models/BlogUser.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogProject.Models
+{
+    public class BlogUser : IdentityUser
+    {
+        [Required]
+
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+        //IMAGE PROPERTIES:
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+
+
+        //SOCIAL MEDIA URLS:
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        public string FacebookUrl { get; set; }
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        public string TwitterXUrl { get; set; }
+
+
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+
+        //NAVIGATION PROPERTIES:
+        //For multi-author system tracking the blogs and posts associated with the logged-in user
+        public virtual ICollection<Blog> Blogs { get; set; } = new HashSet<Blog>();
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+    }
+}
