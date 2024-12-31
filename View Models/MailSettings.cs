b058ba232785008ab3259f7dd60527b6fb481ede
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-12-31 13:13:24 -0500

    Add MailSettings View Model class and json secrets. Include Mailkit and Mimekit packages.

---
+++ View Models/MailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogProject.View_Models
+{
+    public class MailSettings
+    {
+        // Store SMTP settings; to configure and use SMTP server (i.e. google)
+        public string? Mail { get; set; }
+        public string? DisplayName { get; set; }
+        public string? MailPassword { get; set; }
+        public string? MailHost { get; set; }
+        public int MailPort { get; set; }
+    }
+}
