8f36b25ec0ef41898ba143d1d2e371bf8ac2ca80
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-12-31 12:59:01 -0500

    Add IBlogEmailSender and EmailService services. Update scafolded code in Register and ResendEmailConfirmation to use IBlogEmailSender

---
+++ Services/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using BlogProject.View_Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System.Net.Mail;
+
+namespace BlogProject.Services
+{
+    public class EmailService : IBlogEmailSender
+    {
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
+        public Task SendContactEmailAsync(string emailForm, string name, string subject, string htmlMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = htmlMessage;
+
+            email.Body = builder.ToMessageBody();
+
+            using var smtp = new MailKit.Net.Smtp.SmtpClient();
+            smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.MailPassword);
+
+            await smtp.SendAsync(email);
+
+            smtp.Disconnect(true);
+
+            // Optional: Add code to allow attachments to be included
+
+        }
+    }
+}
