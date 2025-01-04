5e8f4aa1f6349b57df40040c5e67abc5137e825f
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-01-04 11:40:05 -0500

    Add migration with edited Blog Model property names

---
+++ Data/Migrations/20250104042319_009 EditBlogCreateUpdateNames.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogProject.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _009EditBlogCreateUpdateNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedDate",
+                table: "Blogs",
+                newName: "Updated");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Blogs",
+                newName: "Created");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Updated",
+                table: "Blogs",
+                newName: "UpdatedDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "Blogs",
+                newName: "CreatedDate");
+        }
+    }
+}
