edde4062ea4aac8464fb2297ea750506ed89792b
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-02-26 13:39:07 -0500

    Add Role based Security and Rendering for Blog creation

--- Controllers/BlogsController.cs
+++ Controllers/BlogsController.cs
@@ -49,7 +49,7 @@ namespace BlogProject.Controllers
         }
 
         // GET: Blogs/Create
-        [Authorize]
+        [Authorize(Roles = "Administrator")]
         public IActionResult Create()
         {
             return View();
