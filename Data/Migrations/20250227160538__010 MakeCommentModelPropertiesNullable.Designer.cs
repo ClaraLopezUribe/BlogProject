8267164b0edbc9036567a76fa9865728160e4641
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-02-27 11:07:59 -0500

    Edit Comment Model to make properties nullable. Add Migration and update DB

--- Data/Migrations/20250227160538__010 MakeCommentModelPropertiesNullable.Designer.cs
+++ Data/Migrations/20250227160538__010 MakeCommentModelPropertiesNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlogProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlogProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250227160538__010 MakeCommentModelPropertiesNullable")]
+    partial class _010MakeCommentModelPropertiesNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
