4e3abef849981b7829fea0e6166e4224564364af
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-03-19 18:09:09 -0400

    Update page layout and task list TODOs

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,6 +1,4 @@
-﻿// TODO: Do stuff in cs file - ctrl+\,t to open Task List window
-
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 
