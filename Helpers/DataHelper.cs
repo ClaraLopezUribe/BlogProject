b37c5c6a6be307c5fea514b84393aca0fe43328a
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-05-24 18:18:16 -0400

    Add DataHelper to get an instance of the database and programatically update it when changes occur

---
+++ Helpers/DataHelper.cs
@@ -0,0 +1,17 @@
+﻿using BlogProject.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProject.Helpers
+{
+    public class DataHelper
+    {
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            // Service: an instance of db context
+            var dbContext = svcProvider.GetRequiredService<ApplicationDbContext>();
+
+            // Migration: This is the programatic equivalent of running the command "Update-Database" in Package Manager Console
+            await dbContext.Database.MigrateAsync();
+        }
+    }
+}
