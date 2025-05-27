0c8b66d93090b15ed2f4446b83b5d4ab67793687
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-05-26 21:49:57 -0400

    Allow admins to view all blogs in HomeController
    Commented out filtering for production-ready posts.
    Included related entities (`BlogUser` and `Posts`) to
    ensure all blogs are displayed for administrators.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,9 +31,9 @@ namespace BlogProject.Controllers
             
             var blogs = await _context.Blogs
                 //TODO : Uncomment the following line to only show blogs with posts that are production ready
-                //.Where(b => b.Posts.Any(p => p.ReadyStatus == Enums.ReadyStatus.ProductionReady)) Wrap in if statement to allow admins to see all blogs regardless of ready status
-                //.Include(b => b.BlogUser)
-                //.Include(b => b.Posts)
+                /*.Where(b => b.Posts.Any(p => p.ReadyStatus == Enums.ReadyStatus.ProductionReady))*/ /*Wrap in if statement to allow admins to see all blogs regardless of ready status*/
+                .Include(b => b.BlogUser)
+                .Include(b => b.Posts)
                 .OrderByDescending(b => b.Created)
                 .ToPagedListAsync(pageNumber, pageSize);
             
