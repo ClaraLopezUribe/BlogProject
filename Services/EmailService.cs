88569919df6331a82e01b65d6e1d512ee31bcb57
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-08-20 21:27:26 -0400

    Revert Mailkit SecureSocketOptions selection
    Change back .StartTls after testing with .Auto still triggered error pages onPost from ContactMe and Identity pages

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -28,7 +28,7 @@ namespace BlogProject.Services
             email.Body = builder.ToMessageBody();
 
             using var smtp = new SmtpClient();
-            smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.Auto);
+            smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.StartTls);
             smtp.Authenticate(_mailSettings.Mail, _mailSettings.MailPassword);
 
             await smtp.SendAsync(email);
@@ -50,7 +50,7 @@ namespace BlogProject.Services
             email.Body = builder.ToMessageBody();
 
             using var smtp = new SmtpClient();
-            smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.Auto);
+            smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.StartTls);
             smtp.Authenticate(_mailSettings.Mail, _mailSettings.MailPassword);
 
             await smtp.SendAsync(email);
