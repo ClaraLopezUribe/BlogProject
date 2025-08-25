87379efe9cb4f5e07202878002df49f2cc5e5989
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-08-25 13:12:52 -0400

    Revert "Uncommented Code for connecting to the database"
    This reverts commit f12210d02c8977177e1b8366afa3f5b54d18a8f1.

--- Program.cs
+++ Program.cs
@@ -50,18 +50,17 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
 
 var app = builder.Build();
 
-//// LEARN : I had commented out this section because I thought the implementation was handled starting with the dataService variable. After deploying to Railway, and running across errors with identity pages, I am not sure if that is the case. I need to test this further.
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<ApplicationDbContext>();
+//// REFERENCE SECTION: This section is for reference only implementation handled starting with the dataService variable
+//using (var scope = app.Services.CreateScope()) {
+//    var services = scope.ServiceProvider;
+//    var context = services.GetRequiredService<ApplicationDbContext>();
 
-    // Apply any pending migrations and create the database if it doesn't exist
-    await context.Database.MigrateAsync();
+//    // Apply any pending migrations and create the database if it doesn't exist
+//    await context.Database.MigrateAsync();
 
-    // Run additional data management tasks
-    await DataHelper.ManageDataAsync(scope.ServiceProvider);
-}
+//    // Run additional data management tasks
+//    await DataHelper.ManageDataAsync(scope.ServiceProvider);
+//}
 
 // Get access to registered DataService
 var dataService = app.Services.CreateScope()
