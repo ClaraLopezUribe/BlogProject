47c6982dfccc170d1a4cfb9310228043df395fb1
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-08-26 16:24:44 -0400

    Add new SendEmailAsync Task in IBlogEmailSender to continue troubleshooting errors on POST actions that require Mimekit.
    NOTE: I also confirmed Mailsettings matched the variables on Railway (particularly Mail as the email). If this commit does not resolve errors, I will change the Mail property to Email as noted in the comments

--- Services/IBlogEmailSender.cs
+++ Services/IBlogEmailSender.cs
@@ -5,5 +5,7 @@ namespace BlogProject.Services
     public interface IBlogEmailSender : IEmailSender
     {
         Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage);
+
+        new Task SendEmailAsync(string emailTo, string subject, string htmlMessage);
     }
 }
--- View Models/MailSettings.cs
+++ View Models/MailSettings.cs
@@ -3,7 +3,7 @@
     public class MailSettings
     {
         // Store SMTP settings; to configure and use SMTP server (i.e. google)
-        public string? Mail { get; set; }
+        public string? Mail { get; set; }  // TODO : change model property name to "Email"??? If I remember correctly this Mail conflicts with some other Mail property elsewhere
         public string? DisplayName { get; set; }
         public string? MailPassword { get; set; }
         public string? MailHost { get; set; }
