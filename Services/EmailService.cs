36ac8b050a50387dfaee943167c8cbdc7942e48c
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-08-29 13:22:30 -0400

    Uncomment original SendEmailAsync task in Email Service to retest error

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -36,77 +36,77 @@ namespace BlogProject.Services
             smtp.Disconnect(true);
 
         }
-        // Refactored to mirror ContactPro EmailService
-        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        //// Refactored to mirror ContactPro EmailService
+        //public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        //{
+        //    // Use null coalescing operator to instruct email sender to get the local value, but if null will get the right hand side value // LEARN : Why check local value first and not the Environment value???
+        //    var emailSender = _mailSettings.Mail ?? Environment.GetEnvironmentVariable("Mail");
+
+        //    MimeMessage newEmail = new();
+
+        //    newEmail.Sender = MailboxAddress.Parse(emailSender);
+
+
+        //    // LEARN : was this loop in ContactPro because an email could be sent to a group consisting of multiple emails? is this a feature that could be needed in the BlogProject? If not delete.
+        //    //foreach (var emailAddress in email.Split(";"))
+        //    //{
+        //    //    newEmail.To.Add(MailboxAddress.Parse(emailAddress));
+        //    //}
+
+        //    newEmail.To.Add(MailboxAddress.Parse(email));
+        //    newEmail.Subject = subject;
+
+        //    BodyBuilder emailBody = new();
+        //    emailBody.HtmlBody = htmlMessage;
+
+        //    newEmail.Body = emailBody.ToMessageBody();
+
+        //    // Log into smtp client
+        //    using SmtpClient smtpClient = new();
+
+        //    try
+        //    {
+        //        var host = _mailSettings.MailHost ?? Environment.GetEnvironmentVariable("MailHost");
+        //        var port = _mailSettings.MailPort != 0 ? _mailSettings.MailPort : int.Parse(Environment.GetEnvironmentVariable("MailPort"));
+        //        var password = _mailSettings.MailPassword ?? Environment.GetEnvironmentVariable("MailPassword");
+
+        //        await smtpClient.ConnectAsync(host, port, SecureSocketOptions.StartTls);
+        //        await smtpClient.AuthenticateAsync(emailSender, password);
+
+        //        await smtpClient.SendAsync(newEmail);
+        //        await smtpClient.DisconnectAsync(true);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        var error = ex.Message;
+        //        throw;
+        //    }
+        //}
+
+
+
+        public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
         {
-            // Use null coalescing operator to instruct email sender to get the local value, but if null will get the right hand side value // LEARN : Why check local value first and not the Environment value???
-            var emailSender = _mailSettings.Mail ?? Environment.GetEnvironmentVariable("Mail");
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(emailTo));
+            email.Subject = subject;
 
-            MimeMessage newEmail = new();
+            var builder = new BodyBuilder();
+            builder.HtmlBody = htmlMessage;
 
-            newEmail.Sender = MailboxAddress.Parse(emailSender);
+            email.Body = builder.ToMessageBody();
 
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.MailPassword);
 
-            // LEARN : was this loop in ContactPro because an email could be sent to a group consisting of multiple emails? is this a feature that could be needed in the BlogProject? If not delete.
-            //foreach (var emailAddress in email.Split(";"))
-            //{
-            //    newEmail.To.Add(MailboxAddress.Parse(emailAddress));
-            //}
+            await smtp.SendAsync(email);
 
-            newEmail.To.Add(MailboxAddress.Parse(email));
-            newEmail.Subject = subject;
+            smtp.Disconnect(true);
 
-            BodyBuilder emailBody = new();
-            emailBody.HtmlBody = htmlMessage;
+            // Optional: Add code to allow attachments to be included
 
-            newEmail.Body = emailBody.ToMessageBody();
-
-            // Log into smtp client
-            using SmtpClient smtpClient = new();
-
-            try
-            {
-                var host = _mailSettings.MailHost ?? Environment.GetEnvironmentVariable("MailHost");
-                var port = _mailSettings.MailPort != 0 ? _mailSettings.MailPort : int.Parse(Environment.GetEnvironmentVariable("MailPort"));
-                var password = _mailSettings.MailPassword ?? Environment.GetEnvironmentVariable("MailPassword");
-
-                await smtpClient.ConnectAsync(host, port, SecureSocketOptions.StartTls);
-                await smtpClient.AuthenticateAsync(emailSender, password);
-
-                await smtpClient.SendAsync(newEmail);
-                await smtpClient.DisconnectAsync(true);
-            }
-            catch (Exception ex)
-            {
-                var error = ex.Message;
-                throw;
-            }
         }
-
-
-
-        ////public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
-        ////{
-        ////    var email = new MimeMessage();
-        ////    email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
-        ////    email.To.Add(MailboxAddress.Parse(emailTo));
-        ////    email.Subject = subject;
-
-        ////    var builder = new BodyBuilder();
-        ////    builder.HtmlBody = htmlMessage;
-
-        ////    email.Body = builder.ToMessageBody();
-
-        ////    using var smtp = new SmtpClient();
-        ////    smtp.Connect(_mailSettings.MailHost, _mailSettings.MailPort, SecureSocketOptions.StartTls);
-        ////    smtp.Authenticate(_mailSettings.Mail, _mailSettings.MailPassword);
-
-        ////    await smtp.SendAsync(email);
-
-        ////    smtp.Disconnect(true);
-
-        ////    // Optional: Add code to allow attachments to be included
-
-        ////}
     }
 }
