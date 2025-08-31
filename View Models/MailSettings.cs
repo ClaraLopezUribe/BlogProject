8b6cc86434c1fd0ddfcc10e61556a55fcb855236
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-08-30 20:28:46 -0400

    Make Mailsettings not nullable (except for DisplayName)

--- View Models/MailSettings.cs
+++ View Models/MailSettings.cs
@@ -3,10 +3,10 @@
     public class MailSettings
     {
         // Store SMTP settings; to configure and use SMTP server (i.e. google)
-        public string? Mail { get; set; }  // TODO : change model property name to "Email"??? If I remember correctly this Mail conflicts with some other Mail property elsewhere
+        public string Mail { get; set; }  // TODO : change model property name to "Email"??? If I remember correctly this Mail conflicts with some other Mail property elsewhere
         public string? DisplayName { get; set; }
-        public string? MailPassword { get; set; }
-        public string? MailHost { get; set; }
+        public string MailPassword { get; set; }
+        public string MailHost { get; set; }
         public int MailPort { get; set; }
     }
 }
