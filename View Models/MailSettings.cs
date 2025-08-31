46373b04a5b0459fd193f509d403a0e7728908e3
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-08-30 20:51:59 -0400

    Add data annotation to Mail property in MailSetting View Model

--- View Models/MailSettings.cs
+++ View Models/MailSettings.cs
@@ -1,8 +1,11 @@
-﻿namespace BlogProject.View_Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogProject.View_Models
 {
     public class MailSettings
     {
         // Store SMTP settings; to configure and use SMTP server (i.e. google)
+        [Required]
         public string Mail { get; set; }  // TODO : change model property name to "Email"??? If I remember correctly this Mail conflicts with some other Mail property elsewhere
         public string? DisplayName { get; set; }
         public string MailPassword { get; set; }
