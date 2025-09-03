ec663dfb2b3edb8ae9dabfb33f02a764efd111ad
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2025-09-03 11:26:37 -0400

    Add migration to ApplicationDbContext
    Add required modifiers in BlogUser and ForgotPassword models

--- Data/Migrations/20250903152335__013 AddRequredModifiers.Designer.cs
+++ Data/Migrations/20250903152335__013 AddRequredModifiers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlogProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlogProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250903152335__013 AddRequredModifiers")]
+    partial class _013AddRequredModifiers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
